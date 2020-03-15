fast radiation simulator workign but looks like it gives a bit wrong results

wiggler_radiation/wiggler_radiation.py
@@ -5,7 +5,7 @@ import numpy as np
 class WigglerRadiationSimulator():
     alpha = 1/137
 
-    def __init__(self, wiggler, beam, aperture=None, harmonic=1,
+    def __init__(self, wiggler, beam, mesh, harmonic=1,
                  bessel_cutoff=20):
         self.wiggler = wiggler
         self.beam = beam
@@ -13,7 +13,19 @@ class WigglerRadiationSimulator():
         self.bessel_cutoff = bessel_cutoff
         self.lambda1_um = 1e6*self.wiggler.lambda_wiggler_m\
             / 2/self.beam.gamma**2*self.wiggler.aux_const
-        self.aperture = aperture
+        self.x_range, self.y_range, self.lambda_range = mesh
+
+    @property
+    def n_x(self):
+        return len(self.x_range)
+
+    @property
+    def n_y(self):
+        return len(self.y_range)
+
+    @property
+    def n_lambda(self):
+        return len(self.n_lambda)
 
     def calc_Ih_1el(self, lambda_um, theta_x, theta_y):
         dw = self.lambda1_um/lambda_um-1
@@ -41,17 +53,35 @@ class WigglerRadiationSimulator():
                                  - self.wiggler.K_peak*(sum2+sum3))**2\
             * self.lambda1_um/lambda_um**2
 
-    def calc_x_y_integral_1el(self, lambda_um):
-        if self.aperture is None:
-            raise TypeError('Aperture for this instance of'
-                            ' WigglerRadiationSimulator is not defined.')
-        if self.aperture.mesh_size_1D is None:
-            raise TypeError("Aperture's mesh is not defined.")
-        Ih_vals = [self.calc_Ih_1el(lambda_um, x, y)
-                   if self.aperture.accepted(x, y) else 0 for x, y in
-                   zip(self.aperture.mesh_xs, self.aperture.mesh_ys)]
-        return self.aperture.step**2*sum(Ih_vals)
+    def calc_Ih_on_mesh(self):
+        x_2D = np.tile(self.x_range, (self.n_y, 1))
+        y_2D = np.tile(self.y_range.reshape(-1, 1), (1, self.n_x))
+        r2_2D = x_2D**2+y_2D**2
+        A = self.wiggler.aux_const+self.beam.gamma**2*r2_2D
+        Y = self.harmonic*self.wiggler.K_peak**2/4/A
+        X = 2*self.harmonic*self.beam.gamma*self.wiggler.K_peak*x_2D/A
+        sum1 = 0
+        sum2 = 0
+        sum3 = 0
+        p = -self.bessel_cutoff
+        jv2pm1 = jv(self.harmonic+2*p-1, X)
+        for p in range(-self.bessel_cutoff, self.bessel_cutoff+1):
+            jvpY = jv(p, Y)
+            sum1 += jv(self.harmonic+2*p, X)*jvpY
+            sum2 += jv2pm1*jvpY
+            jv2pp1 = jv(self.harmonic+2*p+1, X)
+            sum3 += jv2pp1*jvpY
+            jv2pm1 = jv2pp1
+        bessel_part = self.alpha*self.harmonic**2*self.beam.gamma**2\
+            * self.wiggler.N_periods**2\
+            / A**2*np.absolute(2*self.beam.gamma*x_2D*sum1
+                               - self.wiggler.K_peak*(sum2+sum3))**2\
+            * self.lambda1_um
+        dw_arr = self.lambda1_um/self.lambda_range-1
+        L = [sinc(self.wiggler.N_periods*(r2_2D+dw*A)/self.wiggler.aux_const)\
+             ** 2 / l**2 for dw, l in zip(dw_arr, self.lambda_range)]
+        L = np.asarray(L)
+        return bessel_part*L
 
-    def dev(self):
 
 