a0b6e337136da89195a60aca829a5241f47edb46
Ihar Lobach <lobachihor1994@gmail.com>
2020-03-15 15:50:28 -0500

    started analyzing directly connected data

--- fur/waveform_reader.py
+++ fur/waveform_reader.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 
-def read_waveform(file_path):
+def read_waveform(file_path, one_channel=False):
     vals = np.fromfile(file_path, dtype=np.float32)
-    ch1 = vals[::2]
-    ch2 = vals[1::2]
-    return ch1, ch2
+    if one_channel:  # one channel (without the comb filter, directly connected)
+        return -vals
+    else:  # two channels (with the comb filter)
+        ch1 = vals[::2]
+        ch2 = vals[1::2]
+        return ch1, ch2
