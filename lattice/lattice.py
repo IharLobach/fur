7eb9af4b8272b958e912ab89a7ce8ace4f167714
Ihar Lobach <lobachihor1994@gmail.com>
2020-03-28 21:58:07 -0500

    fitted transverse beam sizes for shift_03_10_2020

--- lattice/lattice.py
+++ lattice/lattice.py
@@ -1,17 +1,35 @@
 import numpy as np
+import scipy
 import pandas as pd
 import matplotlib.pyplot as plt
-camera_positions = [
-                    313.99411487289865,
-                    850.7097357006688,
-                    1150.6154142505097,
-                    1649.9644633972994,
-                    2313.0210097709055,
-                    2849.9525071571384,
-                    3148.9348982723236,
-                    3648.20091797355
-                    ]
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import BaggingRegressor
+camera_positions = [312.3907724914158,
+                    847.5314240270161,
+                    1146.7606367662588,
+                    1648.5578345089393,
+                    2312.048374490958,
+                    2846.8364422732257,
+                    3146.9275264095036,
+                    3646.7941322228266]
 camera_names = ['M1R', 'M2R', 'M3R', 'M4R', 'M4L', 'M3L', 'M2L', 'M1L']
+acnet_devices_X = ['N:ITC1RSH',
+                   'N:ITC2RSH',
+                   'N:ITC3RSH',
+                   'N:ITC4RSH',
+                   'N:ITC4LSH',
+                   'N:ITC3LSH',
+                   'N:ITC2LSH',
+                   'N:ITC1LSH']
+acnet_devices_Y = ['N:ITC1RSV',
+                   'N:ITC2RSV',
+                   'N:ITC3RSV',
+                   'N:ITC4RSV',
+                   'N:ITC4LSV',
+                   'N:ITC3LSV',
+                   'N:ITC2LSV',
+                   'N:ITC1LSV']
 
 
 def read_lattice_file(lattice_file_path):
@@ -46,39 +64,83 @@ def read_lattice_file(lattice_file_path):
     data_names[0] = data_names[0][:-2]+"cm"
     lattice_df = pd.DataFrame({
         "S_cm": data_chunks[0][0],
-        "Dispersion_X_cm": data_chunks[0][1],
-        "Beta_X_cm": data_chunks[1][1],
-        "Beta_Y_cm": data_chunks[2][1],
+        "Dispersion_cm_X": data_chunks[0][1],
+        "Beta_cm_X": data_chunks[1][1],
+        "Beta_cm_Y": data_chunks[2][1],
         "Alpha_X": data_chunks[3][1],
         "Alpha_Y": data_chunks[4][1]
     })
     return lattice_df
 
 
-def plot_lattice(lattice_df):
-    fig, (ax0, ax1, ax2) = plt.subplots(3, figsize=(20,15))
-    ax0.plot(lattice_df["S_cm"], lattice_df["Beta_X_cm"], label="Beta_X_cm")
-    ax0.plot(lattice_df["S_cm"], lattice_df["Beta_Y_cm"], label="Beta_Y_cm")
-    fs = 16
-    ax0.set_ylabel("Beta_X_cm, Beta_Y_cm", fontsize=fs)
-    ax0.legend()
-    ax1.plot(lattice_df["S_cm"], lattice_df["Dispersion_X_cm"])
-    ax1.set_ylabel("Dispersion_X_cm", fontsize=fs)
-    ax2.plot(lattice_df["S_cm"], lattice_df["Alpha_X"], label="Alpha_X")
-    ax2.plot(lattice_df["S_cm"], lattice_df["Alpha_Y"], label="Alpha_Y")
-    ax2.set_ylabel("Alpha_X, Alpha_Y", fontsize=fs)
-    ax2.set_xlabel("S_cm")
-    ax2.legend()
-    for ax in (ax0, ax1, ax2):
-        for p in camera_positions:
-            ax.axvline(p, color='green')
-    y_pos_annotate = np.mean(ax1.get_ylim())
+def add_vertical_lines_at_camera_positions(ax, color='green'):
+    for p in camera_positions:
+        ax.axvline(p, color='green')
+
+
+def annotate_camera_positions(ax):
+    y_pos_annotate = np.mean(ax.get_ylim())
     for name, p in zip(camera_names, camera_positions):
-        ax1.annotate(name, (p, y_pos_annotate))
+        ax.annotate(name, (p, y_pos_annotate))
+
+
+def plot_lattice(lattice_df):
+    fig, (ax0, ax1) = plt.subplots(2, figsize=(20, 15))
+    ax0.plot(lattice_df["S_cm"], lattice_df["Beta_cm_X"], label="Beta_cm_X")
+    ax0.plot(lattice_df["S_cm"], lattice_df["Beta_cm_Y"], label="Beta_cm_Y")
+    fs = 16
+    ax0.set_ylabel("Beta_cm_X, Beta_cm_Y", fontsize=fs)
+    ax0.legend()
+    ax1.plot(lattice_df["S_cm"], lattice_df["Dispersion_cm_X"])
+    ax1.set_ylabel("Dispersion_cm_X", fontsize=fs)
+    ax1.set_xlabel("S_cm")
+    # ax2.plot(lattice_df["S_cm"], lattice_df["Alpha_X"], label="Alpha_X")
+    # ax2.plot(lattice_df["S_cm"], lattice_df["Alpha_Y"], label="Alpha_Y")
+    # ax2.set_ylabel("Alpha_X, Alpha_Y", fontsize=fs)
+    # ax2.set_xlabel("S_cm")
+    # ax2.legend()
+    for ax in (ax0, ax1):
+        add_vertical_lines_at_camera_positions(ax)
+    annotate_camera_positions(ax1)
     plt.show()
 
 
-def get_cameras_df(lattice_df):
+def __get_sigma_um(beta_cm, e_um, dispersion_cm, dpp):
+    return np.sqrt(e_um*1e4*beta_cm+(1e4*dispersion_cm*dpp)**2)
+
+
+def show_sigma_fit(lattice_df, cameras_df, axis, emittance_um, dpp=None):
+    if (axis == "X") and (dpp is None):
+        raise ValueError("Chosen axis is 'X', but value"
+                         " of 'dpp' was not specified.")
+    elif axis == "X":
+        dispersion_cm = lattice_df["Dispersion_cm_X"]
+        dpp_ = dpp
+    elif axis == "Y":
+        dispersion_cm = 0
+        dpp_ = 0
+    fig, ax = plt.subplots(figsize=(20, 7.5))
+    ax.plot(lattice_df["S_cm"],
+            __get_sigma_um(
+                lattice_df["Beta_cm_"+axis],
+                emittance_um,
+                dispersion_cm,
+                dpp_),
+            label="sigma_um_"+axis)
+    no_m4r = cameras_df[cameras_df["Name"] != "M4R"]
+    add_vertical_lines_at_camera_positions(ax)
+    annotate_camera_positions(ax)
+    ax.plot(no_m4r["S_cm"], no_m4r["Measured_sigma_um_"+axis],
+            marker='o', linestyle='none', label="Measured_sigma_um_"+axis)
+    fs = 16
+    ax.set_ylabel("sigma_um_"+axis, fontsize=fs)
+    ax.legend()
+    return fig, ax
+
+
+def get_cameras_df(lattice_df,
+                   measured_sigma_um_X,
+                   measured_sigma_um_Y):
     cameras_df = pd.DataFrame({
                                 "Name": camera_names,
                                 "S_cm": camera_positions
@@ -87,4 +149,78 @@ def get_cameras_df(lattice_df):
         cameras_df[col] = np.interp(cameras_df["S_cm"],
                                     lattice_df["S_cm"],
                                     lattice_df[col])
+    cameras_df["ACNET_device_X"] = acnet_devices_X
+    cameras_df["ACNET_device_Y"] = acnet_devices_Y
+    cameras_df["Measured_sigma_um_X"] = measured_sigma_um_X
+    cameras_df["Measured_sigma_um_Y"] = measured_sigma_um_Y
     return cameras_df
+
+
+def get_ey_um_least_squares(
+        cameras_df, show_plot=False,
+        n_estimators=10000,
+        n_estimators_to_plot=50):
+    no_m4r = cameras_df[cameras_df["Name"] != "M4R"]
+    x = no_m4r["Beta_cm_Y"]
+    X = x.values.reshape(-1, 1)
+    y = no_m4r["Measured_sigma_um_Y"]**2
+    model = BaggingRegressor(LinearRegression(fit_intercept=False), 
+                             n_estimators=n_estimators,
+                             bootstrap=True)
+    _ = model.fit(X, y)
+    x_model = np.array([0, max(x)])
+    coefs = np.zeros(n_estimators)
+    for i, m in enumerate(model.estimators_):
+        coefs[i] = m.coef_[0]
+    coefs = 1e-4*coefs
+    ey_um_description = pd.Series(coefs).describe()
+    if show_plot:
+        fig, (ax0, ax1) = plt.subplots(2, figsize=(15, 15))
+        ax0.plot(x, y, 'o')
+        ax0.set_ylim(0, ax0.get_ylim()[1])
+        ax0.set_ylabel("Measured_sigma_um_Y^2")
+        ax0.set_xlabel("Beta_cm_Y")
+        for x_, y_, name in zip(x, y, no_m4r["Name"]):
+            ax0.annotate(name, (x_, y_))
+        for c in np.random.choice(1e4*coefs, size=n_estimators_to_plot):
+            ax0.plot(x_model, c*x_model, color='grey', alpha=0.2)
+        ax0.set_title("Bootstrapping for vertical emittance")
+        sns.boxenplot(coefs, ax=ax1)
+        ax1.set_xlabel("Vertical emittance, um")
+        s = "{}".format(ey_um_description)
+        lines = s.split('\n')[:-1]
+        s = ''
+        for l in lines:
+            s += l+'\n'
+        ax1.text(0.9, 0.5, s,
+                 verticalalignment='bottom', horizontalalignment='right',
+                 transform=ax1.transAxes, fontsize=15)
+        ax1.set_xlim(0, ax1.get_xlim()[1])
+        plt.show()
+    return ey_um_description
+
+
+def get_e_um_Y_scipy_curve_fit(cameras_df):
+    no_m4r = cameras_df[cameras_df["Name"] != "M4R"]
+    popt, pcov = scipy.optimize.curve_fit(
+        lambda beta_cm, e_um: np.sqrt(e_um*1e4*beta_cm),
+        no_m4r["Beta_cm_Y"],
+        no_m4r["Measured_sigma_um_Y"])
+    perr = np.sqrt(np.diag(pcov))[0]
+    return popt[0], perr
+
+
+def get_e_um_X_scipy_curve_fit(cameras_df):
+    no_m4r = cameras_df[cameras_df["Name"] != "M4R"]
+
+    def f(beta_cm_dispersion_cm, e_um, dpp):
+        beta_cm, disperison_cm = beta_cm_dispersion_cm
+        return __get_sigma_um(beta_cm, e_um,
+                              disperison_cm, dpp)
+
+    popt, pcov = scipy.optimize.curve_fit(
+        f,
+        no_m4r.loc[:, ["Beta_cm_X", "Dispersion_cm_X"]].values.T,
+        no_m4r["Measured_sigma_um_X"])
+    perr = np.sqrt(np.diag(pcov))
+    return popt, pcov, perr
