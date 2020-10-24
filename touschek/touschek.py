touschek calculation with fixed vacuum chamber diameter

touschek/touschek.py
@@ -29,7 +29,8 @@ CI = interpolate.interp1d(touschek_func_df.iloc[:, 0],
 
 
 def get_LamTska(lattice_df, Vrf, sp, ex, sz, Ibeam,
-                aperture_factor=1.0, gamma=gamma0):
+                aperture_factor=1.0, use_transverse_acceptance_octupole=True, use_constant_transverse_acceptance=True, constant_acceptance_cm=5.0,
+                gamma=gamma0):
     """The result assumes ey=1. So it has to be deveded by sqrt(ey) in units of um"""
     V0 = Vrf
     etas = alpha-1/gamma**2
@@ -38,10 +39,20 @@ def get_LamTska(lattice_df, Vrf, sp, ex, sz, Ibeam,
     nus0 = np.sqrt(q*V0*np.abs(Ks)/2/np.pi/me/gamma)
     nus = nus0*np.sqrt(np.cos(phiacc))
     fs = f0*nus
-    dP_Psep = 2*nus0/q/np.abs(etas)*np.sqrt(np.cos(phiacc)
+    dP_Psep = aperture_factor*2*nus0/q/np.abs(etas)*np.sqrt(np.cos(phiacc)
                                             - (np.pi/2-phiacc)*np.sin(phiacc))
-    Gs = lamRF/2/np.pi*q/nus*np.abs(etas)
     ldf = lattice_df
+    if use_transverse_acceptance_octupole:
+        daccL = ldf['daccL'].values
+        dP_Psep = np.where(daccL < dP_Psep, daccL, dP_Psep)
+    if use_constant_transverse_acceptance:
+        daccL = \
+        [(constant_acceptance_cm\
+            / (np.sqrt(ldf.loc[i, 'H']*ldf['Beta_cm_X'])
+               + np.abs(ldf['Dispersion_cm_X']))).min() for i in ldf.index]
+        daccL = np.array(daccL)
+        dP_Psep = np.where(daccL < dP_Psep, daccL, dP_Psep)
+    Gs = lamRF/2/np.pi*q/nus*np.abs(etas)
     ldf['Sigma_um_X'] = lattice.get_sigma_um(ldf['Beta_cm_X'], ex,
                                              ldf['Dispersion_cm_X'], sp)
     em = dP_Psep**2*ldf['Beta_cm_X']/gamma**2/ex/1e-4\